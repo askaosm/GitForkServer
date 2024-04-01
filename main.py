dfd86354764a736b284ee8e737a0f1ff3ea3dda7
askaosm <ask.osm08@gmail.com>
2024-04-01 18:42:01 +0900

    Add Hello world 3

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ print("Hello world 1 in the team leader server")
 print("Hello world 2 in the team leader server")
 
 print("Hello world 3 in the team leader server")
-print("Hello world 4 in the team leader server")
+print("Hello world 4")
